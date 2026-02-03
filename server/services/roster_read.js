0b8fa180e584cc88e6ad8518d16cbead4d8e9b62
asimons92 <anthonyallynsimons@gmail.com>
2026-02-03 10:03:11 -0700

    name parsing function in roster_read working

--- server/services/roster_read.js
+++ server/services/roster_read.js
@@ -1,11 +1,27 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const z = require('zod');
+const { parseFullName } = require('../schemas/student.schema');
+
+
 const results = [];
 
 fs.createReadStream('p1roster.csv')
-    .pipe(csv({ skipLines: 6}))
-    .on('data', (data) => results.push(data))
+    .pipe(csv({ skipLines: 6 }))
+    .on('data', (row) => {
+        const { fullName, firstName, lastName} = parseFullName(row['First Middle Last']);
+        const student = {
+            fullName: fullName,
+            firstName: firstName,
+            lastName: lastName,
+            studentID: row['Student ID'],
+            Grade: row['Grade'],
+            Gender: row['Gender']
+        };
+        results.push(student);
+    })
     .on('end', () => {console.log(results);
 
     })
+
+
